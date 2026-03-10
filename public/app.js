a8bc6ebc12640d6a334d684a48f87381d8f52f23
Drelka <47503020+Drelka@users.noreply.github.com>
2026-03-10 01:25:35 +0000

    add full HTML structure and CSS styling

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const age = document.querySelector('#age');
+const weight = document.querySelector('#weight');
+const height = document.querySelector('#height');
+const goal = document.querySelector('#goal');
+const activityLevel = document.querySelector('#activity-level');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+})

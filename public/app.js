b3a3ddb746c35b1baaf38370039f0463c9a3bfdc
Drelka <47503020+Drelka@users.noreply.github.com>
2026-03-10 12:17:59 +0000

    add pre-fill profile form from localStorage

--- public/app.js
+++ public/app.js
@@ -1,12 +1,33 @@
 const form = document.querySelector('form');
-const name = document.querySelector('#name');
+const username = document.querySelector('#username');
 const age = document.querySelector('#age');
 const weight = document.querySelector('#weight');
 const height = document.querySelector('#height');
 const goal = document.querySelector('#goal');
 const activityLevel = document.querySelector('#activity-level');
 
+let storedProfile = localStorage.getItem('profile');
+let profile = JSON.parse(storedProfile) || {};
+if (profile.username) {
+    username.value = profile.username;
+    age.value = profile.age;
+    weight.value = profile.weight;
+    height.value = profile.height;
+    goal.value = profile.goal;
+    activityLevel.value = profile.activityLevel;
+}
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    profile = {};
 
-})
+    profile.username = username.value;
+    profile.age = age.value;
+    profile.weight = weight.value;
+    profile.height = height.value;
+    profile.goal = goal.value;
+    profile.activityLevel = activityLevel.value;
+
+    localStorage.setItem('profile', JSON.stringify(profile));
+});
+

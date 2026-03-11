ff0162661d6d3533f2079f6d988a660dfec05e95
Drelka <47503020+Drelka@users.noreply.github.com>
2026-03-11 10:45:35 +0000

    add calculateCalories fn

--- public/bmr.js
+++ public/bmr.js
@@ -1,7 +1,23 @@
-let basalMetabolicRate;
 
-if (profile.gender === 'male') {
-    basalMetabolicRate = 10*profile.weight + 6.25*profile.height - 5*profile.age + 5;
-} else {
-    basalMetabolicRate = 10*profile.weight + 6.25*profile.height - 5*profile.age - 161;
+const calculateCalories = (profile) => {
+    let basalMetabolicRate;
+
+    if (profile.gender === 'male') {
+        basalMetabolicRate = 10 * profile.weight + 6.25 * profile.height - 5 * profile.age + 5;
+    } else {
+        basalMetabolicRate = 10 * profile.weight + 6.25 * profile.height - 5 * profile.age - 161;
+    }
+
+    const activityMultipliers = {
+        "sedentary": 1.2,
+        "light": 1.375,
+        "moderate": 1.55,
+        "active": 1.725,
+        "very active": 1.9,
+    }
+
+    const totalDailyCalories = basalMetabolicRate * activityMultipliers[profile.activityLevel];
+    return totalDailyCalories;
 }
+
+module.exports = calculateCalories;
